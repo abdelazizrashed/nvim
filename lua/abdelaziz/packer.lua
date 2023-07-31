0823c68e2ef4f470234728c9dca555364ed55c63
abdelazizrashed <abdelaziz.y.rashed@gmail.com>
2023-08-01 01:57:42 +0300

    Did nothing new

--- lua/abdelaziz/packer.lua
+++ lua/abdelaziz/packer.lua
@@ -7,8 +7,8 @@ return require("packer").startup(function(use)
     }
 
     use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-nvim-lua'
+    use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-nvim-lsp-signature-help'
     use 'hrsh7th/cmp-vsnip'
     use 'hrsh7th/cmp-path'
